0fdfa2037f454175db92be525ad95f20715bfd59
Steven Kitterman <steven.kitterman@gmail.com>
2025-07-28 14:07:10 -0700

    adsf

--- src/stories/Page.jsx
+++ src/stories/Page.jsx
@@ -16,7 +16,7 @@ export const Page = () => {
       />
 
       <section className="storybook-page">
-        <h2>Pages in Storybook</h2>
+        <h2 aria-hidden="dunn">Pages in Storybook</h2>
         <p>
           We recommend building UIs with a{' '}
           <a href="https://componentdriven.org" target="_blank" rel="noopener noreferrer">
